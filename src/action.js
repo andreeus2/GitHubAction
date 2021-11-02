38ee5ff294bb9d34883336b6c317791a0d2a5850
andreeus2 <andreeus2@gmail.com>
2021-11-02 17:58:20 -0500

    saying thank you

--- src/action.js
+++ src/action.js
@@ -1,4 +1,19 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
 async function run(){
+    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+    const ocokit = github.getOctokit(GITHUB_TOKEN);
+
+    const {context = {} } = github;
+    const { pull_request } = context.payload;
+
+    await ocokit.issues.createComment({
+        ...context.repo,
+        issues_number: pull_request.number,
+        body: 'Thank you for submitting a pull request'
+    });
+
     console.log('Hello World');
 }
 
