fa694be92e9455c4c075a7799776048fecdadb56
andreeus2 <andreeus2@gmail.com>
2021-11-02 22:11:24 -0500

    try catch v5

--- src/action.js
+++ src/action.js
@@ -1,10 +1,13 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+//const github = require('@actions/github');
+const { Octokit } = require('@octokit/rest');
+
 
 try {
     async function run() {
         const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
-        const octokit = github.getOctokit(GITHUB_TOKEN);
+        //const octokit = github.getOctokit(GITHUB_TOKEN);
+        const octokit = new Octokit();
         console.log(octokit);
         const { context = {} } = github;
         const { pull_request } = context.payload;
