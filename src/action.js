3b2ea1fb5e16d5dde5aaedc989a1928e3ebbfb8e
andreeus2 <andreeus2@gmail.com>
2021-11-02 21:45:21 -0500

    create comment git

--- src/action.js
+++ src/action.js
@@ -3,14 +3,14 @@ const github = require('@actions/github');
 
 async function run(){
     const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
-    const ocokit = github.getOctokit(GITHUB_TOKEN);
+    const octokit = github.getOctokit(GITHUB_TOKEN);
 
-    const {context = {} } = github;
+    const { context = {} } = github;
     const { pull_request } = context.payload;
 
-    await ocokit.issues.createComment({
+    await octokit.rest.issues.createComment({
         ...context.repo,
-        issues_number: pull_request.number,
+        issue_number: pull_request.number,
         body: 'Thank you for submitting a pull request'
     });
 
