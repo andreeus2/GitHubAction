0680213215f01a13d73bcddde4b9c763b4ea04b5
andreeus2 <andreeus2@gmail.com>
2021-11-02 21:51:21 -0500

    try catch v2

--- src/action.js
+++ src/action.js
@@ -1,23 +1,25 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-async function run(){
-    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
-    const octokit = github.getOctokit(GITHUB_TOKEN);
+try {
+    async function run() {
+        const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+        const octokit = github.getOctokit(GITHUB_TOKEN);
 
-    const { context = {} } = github;
-    const { pull_request } = context.payload;
-    try{
-    await octokit.rest.issues.createComment({
-        ...context.repo,
-        issue_number: pull_request.number,
-        body: 'Thank you for submitting a pull request'
-    });
+        const { context = {} } = github;
+        const { pull_request } = context.payload;
 
-    console.log('Hello World');
-    }catch(error){
-        console.log(error);
+        await octokit.rest.issues.createComment({
+            ...context.repo,
+            issue_number: pull_request.number,
+            body: 'Thank you for submitting a pull request'
+        });
+
+        console.log('Hello World');
+
     }
+} catch (error) {
+    console.log(error);
 }
 
 run();
